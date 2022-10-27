791b7984c4ffd1069d3465b957454797d87de191
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-26 23:46:40 -0500

    Entrega

---
+++ TESTDRAWER/src/js/viewModels/apartments.js
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright (c) 2014, 2022, Oracle and/or its affiliates.
+ * Licensed under The Universal Permissive License (UPL), Version 1.0
+ * as shown at https://oss.oracle.com/licenses/upl/
+ * @ignore
+ */
+/*
+ * Your about ViewModel code goes here
+ */
+
+define(["../accUtils"], function (accUtils) {
+    "use strict";
+    function ApartmentsViewModel() {
+      this.connected = () => {
+        accUtils.announce("Apartments page loaded.");
+        document.title = "Apartments";
+      };
+  
+      this.disconnected = () => {
+        // Implement if needed
+      };
+  
+      this.transitionCompleted = () => {
+        // Implement if needed
+      };
+    }
+    return ApartmentsViewModel;
+  });
